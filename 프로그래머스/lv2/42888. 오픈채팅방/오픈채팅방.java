593bc84ae73e1a02347934866d0c0870dfbe5ac1
Psolmin <50397888+Psolmin@users.noreply.github.com>
2022-09-04 23:26:42 +0900

    [level 2] Title: 오픈채팅방, Time: 70.42 ms, Memory: 165 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    private Map<String, String> user = new HashMap<>();
+    private class Chat {
+        String command;
+        String userId;
+
+        public Chat(String command, String userId) {
+            this.command = command;
+            this.userId = userId;
+        }
+
+        @Override
+        public String toString() {
+            switch (command) {
+                case "Enter":
+                    return user.get(userId) + "님이 들어왔습니다.";
+                case "Leave":
+                    return user.get(userId) + "님이 나갔습니다.";
+                default:
+                    return null;
+            }
+        }
+    }
+    public String[] solution(String[] record) {
+//        String[] answer = {};
+        List<String> answer = new ArrayList<>();
+
+        List<Chat> chatList = new ArrayList<>();
+
+        for (String str : record) {
+            String[] command = str.split(" ");
+            if (command.length == 3) {
+                user.put(command[1], command[2]);
+            }
+            chatList.add(new Chat(command[0], command[1]));
+        }
+
+        for (Chat chat : chatList) {
+            String str = chat.toString();
+            if (str != null) {
+                answer.add(str);
+            }
+        }
+
+        return answer.toArray(new String[0]);
+    }
+}
