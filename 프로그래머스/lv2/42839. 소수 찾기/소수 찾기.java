8bd42a039a48807787175812320435be28b4315e
Psolmin <50397888+Psolmin@users.noreply.github.com>
2022-09-21 15:19:58 +0900

    [level 2] Title: 소수 찾기, Time: 14.74 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -2,34 +2,49 @@ import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
-    boolean[] prime;
-    Set<Integer> answer = new HashSet<>();
+//    boolean[] prime;
+    Set<Integer> numSet = new HashSet<>();
 
     public int solution(String numbers) {
-//        int answer = 0;
+        int answer = 0;
 
-        prime = new boolean[(int) Math.pow(10, numbers.length())];
-        prime[0] = prime[1] = true;
-        for (int i = 2; i <= Math.sqrt(prime.length - 1); i++) {
-            if (prime[i]) {
-                continue;
+//        prime = new boolean[(int) Math.pow(10, numbers.length())];
+//        prime[0] = prime[1] = true;
+//        for (int i = 2; i <= Math.sqrt(prime.length - 1); i++) {
+//            if (prime[i]) {
+//                continue;
+//            }
+//
+//            for (int j = i * i; j < prime.length; j += i) {
+//                prime[j] = true;
+//            }
+//        }
+        findAllCase("", numbers);
+        for (Integer i : numSet) {
+            if (i == 2) {
+                answer++;
             }
-
-            for (int j = i * i; j < prime.length; j += i) {
-                prime[j] = true;
+            if (i % 2 != 0 && isPrime(i)) {
+                answer++;
             }
         }
-        findAllCase("", numbers);
-        
-        return answer.size();
-    }
 
+        return answer;
+    }
+    public boolean isPrime(int n){
+        if(n==0 || n==1) return false;
+        for(int i=3; i<=(int)Math.sqrt(n); i+=2){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
     private void findAllCase(String standard, String move) {
         if (standard.length() > 0) {
-            int num = Integer.parseInt(standard);
-            if (!prime[num]) {
-                answer.add(num);
-            }
+            int n = Integer.parseInt(standard);
+//            if (!prime[num]) {
+//                answer.add(num);
+//            }
+            numSet.add(n);
         }
 
         for (int i = 0; i < move.length(); i++) {
