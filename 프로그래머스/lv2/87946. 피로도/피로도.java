2d04376e4f32007f8dfa2496c0d7cef658a143f0
Psolmin <50397888+Psolmin@users.noreply.github.com>
2022-09-22 10:45:18 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -1,21 +1,21 @@
 class Solution {
     int max = Integer.MIN_VALUE;
-
+    boolean[] visit;
     public int solution(int k, int[][] dungeons) {
 //        int answer = -1;
-        boolean[] visit = new boolean[dungeons.length];
-        dfs(dungeons, k, visit, 0);
+        visit = new boolean[dungeons.length];
+        dfs(dungeons, k, 0);
 
         return max;
     }
 
-    private void dfs(int[][] dungeons, int k, boolean[] visit, int clear) {
+    private void dfs(int[][] dungeons, int k, int clear) {
         for (int i = 0; i < dungeons.length; i++) {
             if (visit[i] || k < dungeons[i][0]) {
                 continue;
             }
             visit[i] = true;
-            dfs(dungeons, k - dungeons[i][1], visit, clear + 1);
+            dfs(dungeons, k - dungeons[i][1], clear + 1);
             visit[i] = false;
         }
 
