ee55ad9620494995643bb5bd220c5f1490fa7099
Psolmin <50397888+Psolmin@users.noreply.github.com>
2022-09-23 16:54:48 +0900

    [level 2] Title: 타겟 넘버, Time: 0.45 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,19 +1,19 @@
 class Solution {
     int answer = 0;
-    
     public int solution(int[] numbers, int target) {
         dfs(numbers, target, 0, 0);
-        
         return answer;
     }
-    private void dfs(int[] numbers, int target, int depth, int sum) {
-        if (depth == numbers.length) {
+
+    private void dfs(int[] numbers, int target, int sum, int i) {
+        if (i == numbers.length) {
             if (sum == target) {
                 answer++;
             }
-        } else {
-            dfs(numbers, target, depth + 1, sum + numbers[depth]);
-            dfs(numbers, target, depth + 1, sum - numbers[depth]);
+            return;
         }
+
+        dfs(numbers, target, sum + numbers[i], i + 1);
+        dfs(numbers, target, sum - numbers[i], i + 1);
     }
 }
