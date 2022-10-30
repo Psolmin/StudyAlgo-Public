9069b321e7d50dec81af246af04f2353b5c5bfca
Psolmin <50397888+Psolmin@users.noreply.github.com>
2022-10-30 17:01:14 +0900

    [level 2] Title: 구명보트, Time: 8.45 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -1,14 +1,10 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        // 내림차순으로 정렬, limit 비교하면서 삭제
+        // 정렬 후 큰 것 작은 것 비교
         int answer = 0;
-        
+
         Arrays.sort(people);
         int left = 0;
         int right = people.length - 1;
